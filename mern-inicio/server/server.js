b049cbc36507201c96e416cb8b58b09b337eae2b
Luis Javier <cuh51229184@cuh.mx>
2026-09-04 08:27:35 -0600

    Implement express server with health check endpoint and update package.json to use ES modules

--- mern-inicio/server/server.js
+++ mern-inicio/server/server.js
@@ -1,20 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({
-        Hola: "Luis Javier Hernández"
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-
+import app from './app.js';
+const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`)
 });
