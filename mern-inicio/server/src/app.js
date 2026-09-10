5997fd371070d3074c3723e1fd6f10767cbf17f9
Luis Javier <cuh51229184@cuh.mx>
2026-09-09 21:38:50 -0600

    Actualización del proyecto MERN

--- mern-inicio/server/src/app.js
+++ mern-inicio/server/src/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.get("/salud", (req, res) => {
   res.json({
     estado: "ok",
-    mensaje: "Servidor corriendo correctamente",
+    mensaje: "Hola Mundo Luis Javier Hdez 23",
     fecha: new Date().toISOString(),
   });
 });
