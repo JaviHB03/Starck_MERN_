9e84ba4a6a9e2f3f15bb23bc9ea78a4a05929702
Luis Javier <cuh51229184@cuh.mx>
2026-09-17 08:54:45 -0600

    feat: add initial setup for React application with Vite
    - Added React and Vite configuration files.
    - Created main entry point for the application.
    - Introduced global CSS variables and styles.
    - Included SVG assets for React and Vite logos.

---
+++ mern-inicio/client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+function App() {
+  const materia = "Diseño y Desarrollo de aplicaciones web";
+  const parcial = "Parcial 3";
+  return(
+    <main>
+      <h1> Gestor de tareas </h1>
+      <p> Aplicacion en react </p>
+      <p> Materia:{materia} </p>
+      <p> Parcial:{parcial} </p>
+     </main>
+  ) 
+}
+export default App;
